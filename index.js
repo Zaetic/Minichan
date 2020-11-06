226c62a37bb231ea4fa0a3bb8b4e2630d2069db9
Zaetic <joaogabrielggs@gmail.com>
2020-11-06 16:17:29 -0300

    Minify class and index modified for code style

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-"use strict";
 const chalk = require('chalk');
-const Auth = require("./structures/auth.js");
-const Files = require("./structures/files.js");
-const Mini = require("./structures/minify.js");
+const Auth = require('./structures/auth.js');
+const Files = require('./structures/files.js');
+const Mini = require('./structures/minify.js');
 
 class Minichan {
-    constructor(folder = null){
+    constructor(folder = null) {
         this.local = folder;
         this.localFormat = null;
         this.auth = new Auth(this.local);
@@ -13,18 +12,18 @@ class Minichan {
         this.mini = new Mini();
     }
 
-    async init(){
+    async init() {
         await this.auth.init();
         await this.files.init();
 
         let sFiles = this.files.files;
         sFiles = await this.mini.init(sFiles);
-        sFiles = await this.files.setSizes("new", sFiles);
-        
-        let sizes = await this.files.realSize(sFiles);
-    
-        console.log(`${chalk.bold.greenBright("[Reduction Total]")} ${chalk.bold(sizes.old)} to ${chalk.bold(sizes.new)}`)
-        console.log(`${chalk.bold.greenBright("[Success]")} Result folder ${chalk.bold.yellow(`./dist/${this.files.localFormat}`)}`)
+        sFiles = await this.files.setSizes('new', sFiles);
+
+        const sizes = Files.realSize(sFiles);
+
+        console.log(`${chalk.bold.greenBright('[Reduction Total]')} ${chalk.bold(sizes.old)} to ${chalk.bold(sizes.new)}`);
+        console.log(`${chalk.bold.greenBright('[Success]')} Result folder ${chalk.bold.yellow(`./dist/${this.files.localFormat}`)}`);
     }
 }
 

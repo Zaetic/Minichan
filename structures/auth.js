c799b24e6c53f91c24a13651178541ed002bd7ee
Zaetic <joaogabrielggs@gmail.com>
2020-11-06 16:15:50 -0300

    Auth class modified for code style

--- structures/auth.js
+++ structures/auth.js
@@ -1,27 +1,25 @@
-"use strict";
 const fse = require('fs-extra');
 const chalk = require('chalk');
 
 class Auth {
-    constructor(local){
+    constructor(local) {
         this.local = local;
     }
 
-    async init(){
-        let auth = await this.autentication(this.local);
-        if(auth) throw new Error(chalk.bold.red(auth));
-        else return console.log(`${chalk.bold.greenBright("[Auth]")} Path ok`)
+    async init() {
+        const auth = this.autentication(this.local);
+        if (auth) throw new Error(chalk.bold.red(auth));
+        else return console.log(`${chalk.bold.greenBright('[Auth]')} Path ok`);
     }
 
-    async autentication(local){
-        if(typeof local != 'string') return "Invalid type path";
-        else if(!await this.folderExist(local)) return "Invalid Path";
-        return;
+    autentication(local) {
+        if (typeof local !== 'string') return 'Invalid type path';
+        if (!this.folderExist(local)) return 'Invalid Path';
+        return null;
     }
 
-    async folderExist(local){
-        let exist = fse.pathExistsSync(local);
-        return exist;
+    static folderExist(local) {
+        return fse.pathExistsSync(local);
     }
 }
 

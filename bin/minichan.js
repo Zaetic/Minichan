ce8e6d10871052b66592fcba72455b542aa92e17
Zaetic <joaogabrielggs@gmail.com>
2020-11-23 14:33:10 -0300

    Receives the execution directory by default

--- bin/minichan.js
+++ bin/minichan.js
@@ -4,7 +4,11 @@ const { argv } = require('yargs');
 const Minichan = require('../index.js');
 
 if (argv.mini) {
-    new Minichan().init(argv.mini);
+    if (typeof argv.mini === 'boolean') {
+        new Minichan().init(process.cwd());
+    } else {
+        new Minichan().init(argv.mini);
+    }
 } else {
     console.log("Use 'minichan --mini path'");
 }

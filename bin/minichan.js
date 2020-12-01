95bd48b959e743806e9e1173439c59e26afd9f58
Zaetic <joaogabrielggs@gmail.com>
2020-12-01 13:16:00 -0300

    Update-notifier added

--- bin/minichan.js
+++ bin/minichan.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
 const Minichan = require('../index.js');
 
 yargs
@@ -19,5 +21,8 @@ yargs
 if (yargs.argv._[0] === 'mini') {
     new Minichan().init(yargs.argv.path);
 } else {
-    console.log('Typing minichan --help');
+    console.log('Type minichan --help');
 }
+
+const notifier = updateNotifier({ pkg, updateCheckInterval: 2 });
+if (notifier.update) notifier.notify({ message: `Update available: ${notifier.update.latest} \n Run {updateCommand} to update.` });
